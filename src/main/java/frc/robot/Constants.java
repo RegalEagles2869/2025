89fff975a59045cab8cc630ad987994fa01e1751
Jackson765 <118147028+Jackson765@users.noreply.github.com>
2025-03-02 10:51:44 -0500

    should be good

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,74 +7,85 @@ package frc.robot;
 import edu.wpi.first.units.measure.Time;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final int kOperatorControllerPort = 1;
-    public static final double speedMultiplier = 1;
-    public static final double speedMultiplierSlowMode = .1;
-    public static final double driveDeadBand = .1;
-    public static final double rotationDeadBand = .1;
-  }
-  public static class FieldConstants {
+	public static class OperatorConstants {
+		public static final int kDriverControllerPort = 0;
+		public static final int kOperatorControllerPort = 1;
+		public static final double speedMultiplier = 1;
+		public static final double speedMultiplierSlowMode = .1;
+		public static final double driveDeadBand = .1;
+		public static final double rotationDeadBand = .1;
+	}
 
-  }
-  public static class MotorIDs {
-    public static final int ballGrabber1 = 0;
-    public static final int ballGrabber2 = 0;
-    public static final int elevator = 6;
-    public static final int elevatorFollow = 7;
-    public static final int climberID = 15;
-    public static final int coralIntake = 14;
-    public static final int coralPivot = 15;
-  }
-  public static class ElevatorConstants {
-    public static final double floorPosition = 2;
-    public static final double maxPosition = 10000;
-    public static final double basePosition = 0;
-    public static final double l1Position = 0;
-    public static final double l2Position = 13.690503120422363;
-    public static final double l3Position = 35.83300018310547;
-    // public static final double l4Position = 70.19239807128906;
-    public static final double l4Position = 70.19239807128906;
-    public static final double l4Position2 = l4Position+1;
-    public static final double sourcePosition = 0;
-    public static final double error = 0.2;
-    public static final double stallDetectCur = 40.0;
-    public static final double slowPose = 5.0;
-  }
-  public static class PivotConstants {
-    public static final double floorPosition = 0;
-    public static final double maxPosition = 10000;
+	public static class FieldConstants {
 
-    public static final double basePosition = 0;
-    public static final double l1Position = 0;
-    public static final double l2Position = 0;
-    public static final double l3Position = 0;
-    public static final double l4Position = 0;
-    public static final double sourcePosition = 0;
-    public static final double error = 0;
-  }
-  public static class BallGrabberConstants {
-  }
-  public static class ClimberConstants {
+	}
 
-    public static final double floorPosition = 0;
-    public static final double goodPosition = 250;
-    public static final double maxPosition = 100000;
-    public static final double error = 0;
-  }
-  public static class CoralConstants {
+	public static class MotorIDs {
+		public static final int ballGrabber1 = 0;
+		public static final int ballGrabber2 = 0;
+		public static final int elevator = 6;
+		public static final int elevatorFollow = 7;
+		public static final int climberID = 15;
+		public static final int coralIntake = 14;
+		public static final int coralPivot = 15;
+	}
 
-    public static final double minCurrentRunning = 0;
-    public static final double intakeSpeed = 1.0;
-    public static final double intakeTime = .5;
-  }  
+	public static class ElevatorConstants {
+		public static final double floorPosition = 2;
+		public static final double maxPosition = 10000;
+		public static final double basePosition = 0;
+		public static final double l1Position = 0;
+		public static final double l2Position = 13.690503120422363;
+		public static final double l3Position = 35.83300018310547;
+		// public static final double l4Position = 70.19239807128906;
+		public static final double l4Position = 70.19239807128906;
+		public static final double l4Position2 = l4Position + 1;
+		public static final double sourcePosition = 0;
+		public static final double error = 0.2;
+		public static final double stallDetectCur = 40.0;
+		public static final double slowPose = 5.0;
+	}
+
+	public static class PivotConstants {
+		public static final double floorPosition = 0;
+		public static final double maxPosition = 10000;
+
+		public static final double basePosition = 0;
+		public static final double l1Position = 0;
+		public static final double l2Position = 0;
+		public static final double l3Position = 0;
+		public static final double l4Position = 0;
+		public static final double sourcePosition = 0;
+		public static final double error = 0;
+	}
+
+	public static class BallGrabberConstants {
+	}
+
+	public static class ClimberConstants {
+
+		public static final double floorPosition = 0;
+		public static final double goodPosition = 250;
+		public static final double maxPosition = 100000;
+		public static final double error = 0;
+	}
+
+	public static class CoralConstants {
+
+		public static final double minCurrentRunning = 0;
+		public static final double intakeSpeed = 1.0;
+		public static final double intakeTime = .5;
+	}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,140 +45,153 @@ import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
-        private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
-                                                                                      // speed
-        private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per
-                                                                                          // second
-                                                                                          // max angular velocity
+	private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
+	// speed
+	private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per
+	// second
+	// max angular velocity
 
-        /* Setting up bindings for necessary control of the swerve drive platform */
-        private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-                        .withDeadband(
-                                        MaxSpeed * Constants.OperatorConstants.driveDeadBand
-                                                        * Constants.OperatorConstants.speedMultiplier)
-                        .withRotationalDeadband(MaxAngularRate * Constants.OperatorConstants.rotationDeadBand
-                                        * Inputs.getMultiplier() * Constants.OperatorConstants.speedMultiplier)
-                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
-                                                                                 // motors
-        private final SwerveRequest.FieldCentric driveSlow = new SwerveRequest.FieldCentric()
-                        .withDeadband(MaxSpeed * Constants.OperatorConstants.driveDeadBand
-                                        * Constants.OperatorConstants.speedMultiplierSlowMode)
-                        .withRotationalDeadband(MaxAngularRate * Constants.OperatorConstants.rotationDeadBand
-                                        * Inputs.getMultiplier() * Constants.OperatorConstants.speedMultiplierSlowMode)
-                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
-                                                                                 // motors
-        private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-        private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+	/* Setting up bindings for necessary control of the swerve drive platform */
+	private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+			.withDeadband(
+					MaxSpeed * Constants.OperatorConstants.driveDeadBand
+							* Constants.OperatorConstants.speedMultiplier)
+			.withRotationalDeadband(MaxAngularRate * Constants.OperatorConstants.rotationDeadBand
+					* Inputs.getMultiplier() * Constants.OperatorConstants.speedMultiplier)
+			.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
+	// motors
+	private final SwerveRequest.FieldCentric driveSlow = new SwerveRequest.FieldCentric()
+			.withDeadband(MaxSpeed * Constants.OperatorConstants.driveDeadBand
+					* Constants.OperatorConstants.speedMultiplierSlowMode)
+			.withRotationalDeadband(MaxAngularRate * Constants.OperatorConstants.rotationDeadBand
+					* Inputs.getMultiplier() * Constants.OperatorConstants.speedMultiplierSlowMode)
+			.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
+	// motors
+	private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+	private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-        private final Telemetry logger = new Telemetry(MaxSpeed);
+	private final Telemetry logger = new Telemetry(MaxSpeed);
 
-        private final CommandXboxController joystick = new CommandXboxController(0);
+	private final CommandXboxController joystick = new CommandXboxController(0);
 
-        public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+	public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
-        public RobotContainer() {
-                Field2d field = new Field2d();
-                // Do this in either robot or subsystem init
-                SmartDashboard.putData("Field", field);
-                NamedCommands.registerCommand("TestCommand", new TestCommand());
-                NamedCommands.registerCommand("L1", new L1Coral());
-                NamedCommands.registerCommand("L2", new L2Coral());
-                NamedCommands.registerCommand("L3", new L3Coral());
-                NamedCommands.registerCommand("L4", new L4Coral());
-                NamedCommands.registerCommand("Order1", new L1Coral());
-                NamedCommands.registerCommand("Order2", new L2Coral());
-                NamedCommands.registerCommand("Order3", new L3Coral());
-                NamedCommands.registerCommand("Order4", new L4Coral());
-                NamedCommands.registerCommand("SourceIntake", new SourceIntake());
-                // Do this in either robot periodic or subsystem periodic
-                field.setRobotPose(drivetrain.getNegativePose());
-                configureBindings();
-        }
+	public RobotContainer() {
+		Field2d field = new Field2d();
+		// Do this in either robot or subsystem init
+		SmartDashboard.putData("Field", field);
+		NamedCommands.registerCommand("TestCommand", new TestCommand());
+		NamedCommands.registerCommand("L1", new L1Coral());
+		NamedCommands.registerCommand("L2", new L2Coral());
+		NamedCommands.registerCommand("L3", new L3Coral());
+		NamedCommands.registerCommand("L4", new L4Coral());
+		NamedCommands.registerCommand("Order1", new L1Coral());
+		NamedCommands.registerCommand("Order2", new L2Coral());
+		NamedCommands.registerCommand("Order3", new L3Coral());
+		NamedCommands.registerCommand("Order4", new L4Coral());
+		NamedCommands.registerCommand("SourceIntake", new SourceIntake());
+		// Do this in either robot periodic or subsystem periodic
+		field.setRobotPose(drivetrain.getNegativePose());
+		configureBindings();
+	}
 
-        private void configureBindings() {
-                Inputs.getResetGyro().onTrue(new ResetGyro());
+	private void configureBindings() {
+		Inputs.getResetGyro().onTrue(new ResetGyro());
 
-                Inputs.getElevatorDown().onTrue(new ElevatorToFloor());
-                // Inputs.getElevatorL2().onTrue(new L2Coral());
-                // Inputs.getElevatorL3().onTrue(new L3Coral());
-                // Inputs.getElevatorL4().onTrue(new L4Coral());
-                Inputs.getElevatorL2().onTrue(new SetElevatorPosition(Constants.ElevatorConstants.l2Position));
-                Inputs.getElevatorL3().onTrue(new SetElevatorPosition(Constants.ElevatorConstants.l3Position));
-                Inputs.getElevatorL4().onTrue(new SetElevatorPosition(Constants.ElevatorConstants.l4Position));
-                Inputs.getClimberNeutral().onTrue(new SetClimberPosition(Constants.ClimberConstants.floorPosition));
-                Inputs.getClimberGood().onTrue(new SetClimberPosition(Constants.ClimberConstants.goodPosition));
+		Inputs.getElevatorDown().onTrue(new ElevatorToFloor());
+		// Inputs.getElevatorL2().onTrue(new L2Coral());
+		// Inputs.getElevatorL3().onTrue(new L3Coral());
+		// Inputs.getElevatorL4().onTrue(new L4Coral());
+		Inputs.getElevatorL2().onTrue(new SetElevatorPosition(Constants.ElevatorConstants.l2Position));
+		Inputs.getElevatorL3().onTrue(new SetElevatorPosition(Constants.ElevatorConstants.l3Position));
+		Inputs.getElevatorL4().onTrue(new SetElevatorPosition(Constants.ElevatorConstants.l4Position));
+		// Inputs.getClimberNeutral().onTrue(new SetClimberPosition(Constants.ClimberConstants.floorPosition));
+		// Inputs.getClimberGood().onTrue(new SetClimberPosition(Constants.ClimberConstants.goodPosition));
 
-                Inputs.getElevatorSpeedUp().whileTrue(new SetElevatorSpeed(.2));
-                Inputs.getElevatorSpeedDown().whileTrue(new SetElevatorSpeed(-.2));
-                Inputs.getIntakeIn().whileTrue(new SetIntakeSpeed(-Constants.CoralConstants.intakeSpeed));
-                Inputs.getIntakeOut().whileTrue(new SetIntakeSpeed(Constants.CoralConstants.intakeSpeed));
+		Inputs.getElevatorSpeedUp().whileTrue(new SetElevatorSpeed(.2));
+		Inputs.getElevatorSpeedDown().whileTrue(new SetElevatorSpeed(-.2));
+		Inputs.getIntakeIn().whileTrue(new SetIntakeSpeed(-Constants.CoralConstants.intakeSpeed));
+		Inputs.getIntakeOut().whileTrue(new SetIntakeSpeed(Constants.CoralConstants.intakeSpeed));
 
-                Inputs.getClimberUp().whileTrue(new SetClimberSpeed(1));
-                Inputs.getClimberDown().whileTrue(new SetClimberSpeed(-1));
-                Inputs.getRUMBLE().whileTrue(new RumbleRumble());
-                Inputs.getTest().onTrue(new DriveToPose(new Pose2d(0, 0, new Rotation2d(0))));
+		Inputs.getClimberUp().whileTrue(new SetClimberSpeed(1));
+		Inputs.getClimberDown().whileTrue(new SetClimberSpeed(-1));
+		Inputs.getRUMBLE().whileTrue(new RumbleRumble());
+		Inputs.getTest().onTrue(new DriveToPose(new Pose2d(0, 0, new Rotation2d(0))));
 
-                // Note that X is defi,m ned as forward according to WPILib convention,
-                // and Y is defined as to the left according to WPILib convention.
+		// Note that X is defi,m ned as forward according to WPILib convention,
+		// and Y is defined as to the left according to WPILib convention.
 
-                 drivetrain.setDefaultCommand(
-                 // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() ->
-                 drive.withVelocityX(Inputs.getTranslationX() * MaxSpeed *
-                 Constants.OperatorConstants.speedMultiplier) // Drive forward with negative Y
-                //  (forward)
-                 .withVelocityY(Inputs.getTranslationY() * MaxSpeed *
-                 Constants.OperatorConstants.speedMultiplier) // Drive left with negative X
-                (left)
-                .withRotationalRate(Inputs.getRotation() * MaxAngularRate *
-                Constants.OperatorConstants.speedMultiplier) // Drive counterclockwise with
-                //  negative X (left)
-                  ));
+		// drivetrain.setDefaultCommand(
+		// // Drivetrain will execute this command periodically
+		// drivetrain.applyRequest(() ->
+		// drive.withVelocityX(Inputs.getTranslationX() * MaxSpeed *
+		// Constants.OperatorConstants.speedMultiplier) // Drive forward with negative Y
+		// // (forward)
+		// .withVelocityY(Inputs.getTranslationY() * MaxSpeed *
+		// Constants.OperatorConstants.speedMultiplier) // Drive left with negative X
+		// (left)
+		// .withRotationalRate(Inputs.getRotation() * MaxAngularRate *
+		// Constants.OperatorConstants.speedMultiplier) // Drive counterclockwise with
+		// // negative X (left)
+		// ));
+		drivetrain.setDefaultCommand(
+				// Drivetrain will execute this command periodically
+				drivetrain.applyRequest(() -> drive
+						.withVelocityX(
+								Inputs.getTranslationX() * MaxSpeed * Constants.OperatorConstants.speedMultiplier)
+						// Drive forward with negative Y (forward)
+						.withVelocityY(
+								Inputs.getTranslationY() * MaxSpeed * Constants.OperatorConstants.speedMultiplier)
+						// Drive left with negative X (left)
+						.withRotationalRate(
+								Inputs.getRotation() * MaxAngularRate * Constants.OperatorConstants.speedMultiplier)
+				// Drive counterclockwise with negative X (left)
+				));
 
-                joystick.x().whileTrue(
-                                drivetrain.applyRequest(() -> driveSlow
-                                                .withVelocityX(Inputs.getTranslationX() * MaxSpeed
-                                                                * Constants.OperatorConstants.speedMultiplierSlowMode) // Drive
-                                                                                                                       // forward
-                                                                                                                       // with
-                                                                                                                       // negative
-                                                                                                                       // Y
-                                                                                                                       // (forward)
-                                                .withVelocityY(Inputs.getTranslationY() * MaxSpeed
-                                                                * Constants.OperatorConstants.speedMultiplierSlowMode) // Drive
-                                                                                                                       // left
-                                                                                                                       // with
-                                                                                                                       // negative
-                                                                                                                       // X
-                                                                                                                       // (left)
-                                                .withRotationalRate(Inputs.getRotation() * MaxAngularRate
-                                                                * Constants.OperatorConstants.speedMultiplierSlowMode) // Drive
-                                                                                                                       // counterclockwise
-                                                                                                                       // with
-                                                                                                                       // negative
-                                                                                                                       // X
-                                                                                                                       // (left)
-                                ));
-                joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-                joystick.b().whileTrue(drivetrain.applyRequest(
-                                () -> point.withModuleDirection(
-                                                new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+		joystick.x().whileTrue(
+				drivetrain.applyRequest(() -> driveSlow
+						.withVelocityX(Inputs.getTranslationX() * MaxSpeed
+								* Constants.OperatorConstants.speedMultiplierSlowMode) // Drive
+						// forward
+						// with
+						// negative
+						// Y
+						// (forward)
+						.withVelocityY(Inputs.getTranslationY() * MaxSpeed
+								* Constants.OperatorConstants.speedMultiplierSlowMode) // Drive
+						// left
+						// with
+						// negative
+						// X
+						// (left)
+						.withRotationalRate(Inputs.getRotation() * MaxAngularRate
+								* Constants.OperatorConstants.speedMultiplierSlowMode) // Drive
+				// counterclockwise
+				// with
+				// negative
+				// X
+				// (left)
+				));
+		joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+		joystick.b().whileTrue(drivetrain.applyRequest(
+				() -> point.withModuleDirection(
+						new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
 
-                // Run SysId routines when holding back/start and X/Y.
-                // Note that each routine should be run exactly once in a single log.
-                joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-                joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-                joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-                joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+		// Run SysId routines when holding back/start and X/Y.
+		// Note that each routine should be run exactly once in a single log.
+		joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+		joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+		joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+		joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-                // reset the field-centric heading on left bumper press
-                joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+		// reset the field-centric heading on left bumper press
+		joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-                drivetrain.registerTelemetry(logger::telemeterize);
-        }
+		drivetrain.registerTelemetry(logger::telemeterize);
+	}
 
-        public Command getAutonomousCommand() {
-                // return drivetrain.moveTo(new Pose2d(1, 0, new Rotation2d(0)));
-                return new WaitCommand(.1);
-        }
+	public Command getAutonomousCommand() {
+		// return drivetrain.moveTo(new Pose2d(1, 0, new Rotation2d(0)));
+		return new WaitCommand(.1);
+	}
 }
